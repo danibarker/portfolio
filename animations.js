92919fcef7b8f4205915c18498258969ab96662d
emmalee james <mx.emmalee.james@gmail.com>
2020-09-27 11:00:53 -0600

    new version

--- animations.js
+++ animations.js
@@ -12,55 +12,60 @@
     // });
     // });
     // fade animation
-    $(document).ready(function() {
+$(document).ready(function () {
+    $("body").css("font-size", Math.sqrt($("section").width() * $("section").height())/25);
 
-    console.log('ready');
-    $('div.parallax').scroll( function(){
-        
-        $(".fades").each( function(i){
-            let bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            let top_of_window = $(window).scrollTop();
-            let bottom_of_window = $(window).scrollTop()+$(window).height();
-            let top_of_object = $(this).offset().top;
+    $(window).resize(function () {
+        $("body").css("font-size", Math.sqrt($("section").width() * $("section").height())/25);
+    })
+        $(window).scroll( function(){
             
-            if( (top_of_window > bottom_of_object || top_of_object > bottom_of_window) && $(this).hasClass("fadein")){
+            
+            $(".fades").each( function(i){
+                let bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                let top_of_window = $(window).scrollTop();
+                let bottom_of_window = $(window).scrollTop()+$(window).height();
+                let top_of_object = $(this).offset().top;
                 
-                $(this).removeClass('fadein');
-                $(this).addClass('fadeout')
+                if( (top_of_window > bottom_of_object || top_of_object > bottom_of_window) && $(this).hasClass("fadein")){
                     
-            } 
-            
-            else if (top_of_window < bottom_of_object && top_of_object < bottom_of_window && $(this).hasClass("fadeout")){
-                $(this).removeClass('fadeout');
-                $(this).addClass('fadein');
+                    $(this).removeClass('fadein');
+                    $(this).addClass('fadeout')
+                        
+                } 
+                
+                else if (top_of_window < bottom_of_object && top_of_object < bottom_of_window && $(this).hasClass("fadeout")){
+                    $(this).removeClass('fadeout');
+                    $(this).addClass('fadein');
+                }
+                
+            });
+    
+            if ($(window).scrollTop() > $('#nav').offset().top) {
+                $('#nav').removeClass('fadein');
+                $('#nav').addClass('fadeout');
+                $('#navside').removeClass('fadeout');
+                $('#navside').addClass('fadein');
+            } else {
+                $('#navside').removeClass('fadein');
+                $('#navside').addClass('fadeout');
+                $('#nav').removeClass('fadeout');
+                $('#nav').addClass('fadein');
             }
             
+            var scrollPos = $(document).scrollTop();
+            $('#navside a').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos+200 && refElement.position().top + refElement.height() > scrollPos+200) {
+                    $('a').removeClass("active");
+                    currLink.addClass("active");
+                }
+                else{
+                    currLink.removeClass("active");
+                }
+            });
         });
-
-        if ($(window).scrollTop() > $('#nav').offset().top) {
-            $('#nav').removeClass('fadein');
-            $('#nav').addClass('fadeout');
-            $('#navside').removeClass('fadeout');
-            $('#navside').addClass('fadein');
-        } else {
-            $('#navside').removeClass('fadein');
-            $('#navside').addClass('fadeout');
-            $('#nav').removeClass('fadeout');
-            $('#nav').addClass('fadein');
-        }
         
-        var scrollPos = $(document).scrollTop();
-        $('#navside a').each(function () {
-            var currLink = $(this);
-            var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPos+200 && refElement.position().top + refElement.height() > scrollPos+200) {
-                $('a').removeClass("active");
-                currLink.addClass("active");
-            }
-            else{
-                currLink.removeClass("active");
-            }
-        });
     });
     
-});
